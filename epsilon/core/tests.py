1ff53f161bc54bd70e385c066fee0bd1e2a40178
Ulysses Almeida <ulysses.almeida@gmail.com>
2018-04-21 16:39:17 -0400

    Deixando testes mais organizado

--- epsilon/core/tests.py
+++ epsilon/core/tests.py
@@ -2,8 +2,13 @@ from django.test import TestCase
 
 # Create your tests here.
 class MainPageTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
     def test_get(self):
         'GET deve retornar codigo de status 200'
-        response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Página inicial deve usar template index.html'
+        self.assertTemplateUsed(self.resp, 'index.html')
