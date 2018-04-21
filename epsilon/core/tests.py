6967e65c12ab7ca681bad037d86ff75340046b7c
Ulysses Almeida <ulysses.almeida@gmail.com>
2018-04-21 16:27:37 -0400

    Criado testes para página inicial

--- epsilon/core/tests.py
+++ epsilon/core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class MainPageTest(TestCase):
+    def test_get(self):
+        'GET deve retornar codigo de status 200'
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
