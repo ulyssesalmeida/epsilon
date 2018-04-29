2cc2e89c2c2130831613e35a6fd5a0aa16e1133b
Ulysses Almeida <ulysses.almeida@gmail.com>
2018-04-29 15:41:29 -0400

    Definido modelo para armazenar uma proposta inicial de projeto

--- epsilon/projects/models.py
+++ epsilon/projects/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Pip(object):
-    def __init__(self,**kwargs):
-        pass
+class Pip(models.Model):
+    title = models.CharField(max_length=90)
+    orgUnit = models.CharField(max_length=10)
+    client = models.CharField(max_length=30)
+    justification = models.TextField()
+    objectives = models.TextField()
+    cost_estimates = models.CharField(max_length=60)
