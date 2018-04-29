a03c4cffb14d4cccf42897ae8a5b49252e541cab
Ulysses Almeida <ulysses.almeida@gmail.com>
2018-04-29 14:31:34 -0400

    Enviado formulario do PIP no contexto da view do PIP

--- epsilon/projects/views.py
+++ epsilon/projects/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from epsilon.projects.forms import PipForm
 
 # Create your views here.
 def pip(request):
-    return render(request, "projects/pip_form.html")
+    return render(request,
+        template_name = "projects/pip_form.html",
+        context = {'form':PipForm()})
