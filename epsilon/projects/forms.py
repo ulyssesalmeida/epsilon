bb22e10b5d113e04cdc6a7ef23807ee2e0dc5ff1
Ulysses Almeida <ulysses.almeida@gmail.com>
2018-04-29 15:01:07 -0400

    Criado formulário inicial para PIP. Criado campo de formulário na template pip_form.html

--- epsilon/projects/forms.py
+++ epsilon/projects/forms.py
@@ -1,3 +1,9 @@
+from django import forms
 
-class PipForm(object):
-    pass
+class PipForm(forms.Form):
+    title = forms.CharField()
+    orgUnit = forms.CharField()
+    client = forms.CharField()
+    justification = forms.CharField(widget=forms.Textarea)
+    objectives = forms.CharField(widget=forms.Textarea)
+    cost_estimates = forms.CharField()
